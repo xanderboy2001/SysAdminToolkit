bcc632bff87603aeee6ea1355298b53f9d9128cd
Alexander Christian <alexander.christian@gmail.com>
2025-11-21 16:46:37 -0500

    Started script to unlock AD accounts. Right now just gets AD user and checks if account is locked

---
+++ Modules/SysAdminToolkit/Server/ActiveDirectory/Scripts/Unlock-ADAccount.ps1
@@ -0,0 +1,82 @@
+function Convert-UsernameFormat {
+    param([String]$Username)
+
+    $Username = $Username.ToLower()
+
+    # If username is email address (contains '@' symbol), remove everything after the '@'
+    if ($Username -like '*@*') {
+        $Username = $Username -replace '@.*$', ''
+    }
+
+    return $Username
+}
+
+function Read-Username {
+    while ($true) {
+        $usernameInput = Read-Host -Prompt 'Enter the username (or full name) of the user whose Active Directory account you wish to unlock'
+        $username = Convert-UsernameFormat -Username $usernameInput
+
+        if ($username -match '^[a-z]+\.[a-z]+$' -or $username -match '^[a-z]+ [a-z]+$') {
+            return $username
+        }
+
+        Write-Host "Invalid input must be in either format: 'first.last', 'first last'" -ForegroundColor Red
+    }
+}
+
+function Get-ValidADUser {
+    while ($true) {
+        $username = Read-Username
+
+        if ($username -match '^[a-z]+\.[a-z]+$') {
+            try {
+                $userAccount = Get-ADUser -Identity $username -ErrorAction Stop
+                return $userAccount
+            }
+            catch {
+                Write-Host "No Active Directory user found with username '$username'." -ForegroundColor Red
+                Write-Host 'Please try again.' -ForegroundColor Yellow
+            }
+        }
+
+        if ($username -match '^[a-z]+ [a-z]+$') {
+
+            $userAccount = Get-ADUser -Filter "Name -eq '$username'" -ErrorAction SilentlyContinue
+
+            if ($null -eq $userAccount) {
+                Write-Host "No Active Directory user found with name '$username'." -ForegroundColor Red
+                Write-Host 'Please try again.' -ForegroundColor Yellow
+                continue
+            }
+
+            if ($userAccount.Count -gt 1) {
+                Write-Host "Multiple users found matching '$username':" -ForegroundColor Yellow
+                $userAccount | Select-Object -ExpandProperty DisplayName | Sort-Object | ForEach-Object {
+                    Write-Host " - $_" -ForegroundColor Cyan
+                }
+                Write-Host 'Please refine your input.' -ForegroundColor Yellow
+                continue
+            }
+
+            return $userAccount
+        }
+    }
+}
+
+
+Show-MenuHeader -Title 'Unlock Active Directory Account'
+Write-Host @'
+This script takes an Active Directory account username as input and checks if it is locked.
+If locked, the script will prompt for confirmation before unlocking the account.
+'@ -ForegroundColor Yellow
+
+$userAccount = Get-ValidADUser
+
+$isLocked = (Get-ADUser -Identity $userAccount.SamAccountName -Properties 'LockedOut').LockedOut
+
+if ($isLocked) {
+    Write-Host "$($userAccount.SamAccountName) is locked"
+}
+else {
+    Write-Host "$($userAccount.SamAccountName) is not locked"
+}
