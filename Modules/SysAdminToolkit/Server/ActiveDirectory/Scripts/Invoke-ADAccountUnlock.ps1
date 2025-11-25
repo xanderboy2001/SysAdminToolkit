df150fa8e4ccc321cb6c6b879bbface4263cfa80
Alexander Christian <alexander.christian@gmail.com>
2025-11-25 16:30:22 -0500

    Wrapped into function so dot-sourcing doesnt run the whole script

--- Modules/SysAdminToolkit/Server/ActiveDirectory/Scripts/Invoke-ADAccountUnlock.ps1
+++ Modules/SysAdminToolkit/Server/ActiveDirectory/Scripts/Invoke-ADAccountUnlock.ps1
@@ -63,27 +63,28 @@ function Get-ValidADUser {
     }
 }
 
-
-Show-MenuHeader -Title 'Unlock Active Directory Account'
-Write-Host @'
+function Invoke-ADAccountUnlock {
+    Show-MenuHeader -Title 'Unlock Active Directory Account'
+    Write-Host @'
 This script takes an Active Directory account username as input and checks if it is locked.
 If locked, the script will prompt for confirmation before unlocking the account.
 '@ -ForegroundColor Yellow
 
-$userAccount = Get-ValidADUser
-$username = $userAccount.SamAccountName
+    $userAccount = Get-ValidADUser
+    $username = $userAccount.SamAccountName
 
-$isLocked = (Get-ADUser -Identity $userAccount.SamAccountName -Properties 'LockedOut').LockedOut
+    $isLocked = (Get-ADUser -Identity $userAccount.SamAccountName -Properties 'LockedOut').LockedOut
 
-if ($isLocked) {
-    Write-Host "$username is locked" -ForegroundColor Yellow
-    Write-Host "Ensure you verify the user's identity via security questions or manager approval." -ForegroundColor Yellow
-    $action = "Unlock AD account for $username"
-    if (Confirm-UserChoice -Action $action) {
-        Unlock-ADAccount -Identity $username
-        Write-Host "$username has been unlocked" -ForegroundColor Green
+    if ($isLocked) {
+        Write-Host "$username is locked" -ForegroundColor Yellow
+        Write-Host "Ensure you verify the user's identity via security questions or manager approval." -ForegroundColor Yellow
+        $action = "Unlock AD account for $username"
+        if (Confirm-UserChoice -Action $action) {
+            Unlock-ADAccount -Identity $username
+            Write-Host "$username has been unlocked" -ForegroundColor Green
+        }
     }
-}
-else {
-    Write-Host "$username is not locked" -ForegroundColor Yellow
+    else {
+        Write-Host "$username is not locked" -ForegroundColor Yellow
+    }
 }
