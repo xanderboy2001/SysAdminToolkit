886f80ac4bd5b90977f0a926e568fdbc98637435
Alexander Christian <alexander.christian@gmail.com>
2025-12-23 12:14:43 -0500

    Implement Invoke-ADSync function to run AD Delta Sync

--- Public/Server/ActiveDirectory/Invoke-ADSync.ps1
+++ Public/Server/ActiveDirectory/Invoke-ADSync.ps1
@@ -1,4 +1,24 @@
-#TODO Connect to ADConnect with Enter-PSSession -ComputerName "ADConnect" -Credential (Get-Credential)
-#TODO Start ADSync with Start-ADSyncSyncCycle
-#TODO Wait until (Get-ADSyncConnectorRunStatus).RunState != 'Busy'
-#TODO report end of AD Sync and exit PS Session with Exit-PSSession
+
+function Invoke-ADSync {
+    Show-MenuHeader -Title 'Run AD Delta Sync'
+    Write-Host 'This script triggers an Active Directory Delta sync.' -ForegroundColor Yellow
+
+    Write-Host "Connecting to 'ADConnect' server and starting sync..."
+
+    Invoke-Command -ComputerName 'ADConnect' -ScriptBlock {
+        Import-Module ADSync
+        Start-ADSyncSyncCycle -PolicyType Delta
+        Write-Host 'Waiting for sync to start' -NoNewline -ForegroundColor Yellow
+        do {
+            Write-Host '...' -NoNewline -ForegroundColor Yellow
+            Start-Sleep -Seconds 1
+        } while ((Get-ADSyncConnectorRunStatus).RunState -ne 'Busy')
+
+        Write-Host "`nAD sync started. Waiting until completion" -NoNewline -ForegroundColor Yellow
+        do {
+            Write-Host '...' -NoNewline -ForegroundColor Yellow
+            Start-Sleep -Seconds 1
+        } while ((Get-ADSyncConnectorRunStatus).RunState -eq 'Busy')
+        Write-Host "`nAD Sync complete" -ForegroundColor Green
+    } | Out-Null
+}
